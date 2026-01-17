a9a446881cbd93638b8bae8bfc6417e367fdf74d
1-Lewis <b00815051@eessec.edu>
2026-01-17 12:31:59 +0100

    Fix unused locale in school admin page

--- app/admin/school/page.tsx
+++ app/admin/school/page.tsx
@@ -15,7 +15,7 @@ export default async function SchoolAdminPage() {
         redirect('/teacher/courses')
     }
 
-    const locale = await getLocale()
+    await getLocale()
     const dictionary = await getDictionary()
 
     let institutionId = session.user.institutionId
